0a987064014e4c3ea9b365237f75135080a5d6a1
Ahmed <ahmedelshalawy556@gmail.com>
2024-04-16 20:42:47 +0200

    creating maven project

---
+++ src/test/java/com/learnmaven/maveninfewsteps/MavenInFewStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnmaven.maveninfewsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenInFewStepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
